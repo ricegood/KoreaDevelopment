27ff42df0b58c88b14d00a3a14fdf22dc8deb8f5
ricegood <951227@gmail.com>
2017-02-01 08:02:17 +0900

    distance set

--- Assets/Road.cs
+++ Assets/Road.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Road : MonoBehaviour {
 	public GameObject[] city = new GameObject[2];
+	public int distance;
 
 	// Use this for initialization
 	void Start () {
