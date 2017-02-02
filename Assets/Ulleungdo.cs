848fd5f3dc6b935ed8e7aebe4c8b25615a077909
ricegood <951227@gmail.com>
2017-02-02 16:25:03 +0900

    Ulleungdo & Dokdo Color

---
+++ Assets/Ulleungdo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ulleungdo : MonoBehaviour {
+	public SpriteRenderer thisMap;
+	public SpriteRenderer Kyeongbuk;
+
+	// Use this for initialization
+	void Start () {
+		thisMap.color = Kyeongbuk.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		thisMap.color = Kyeongbuk.color;
+	}
+}
