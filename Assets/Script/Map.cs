7be7980da6bea238d3abe5379195abad91465741
ricegood <951227@gmail.com>
2017-02-04 16:23:33 +0900

    ~

--- Assets/Script/Map.cs
+++ Assets/Script/Map.cs
@@ -59,19 +59,6 @@ public class Map : MonoBehaviour {
 			return false;
 	}
 
-	/*
-	public static bool isConnected(City a, City b){
-		return false;
-	}
-
-	private void setCityFlag(bool b){
-		for(int i=0; i<city.Length; i++){
-			City thisCity = city [i].GetComponent<City> ();
-			thisCity.setFlag (b);
-		}
-	}
-	*/
-
 	public static Road getRoad(City a, City b){
 		for (int i = 0; i < a.roadList.Length; i++) {
 			if (b == a.roadList [i].GetComponent<Road> ().getAdgacencyCity (a)) {
