f766a43bc51e6e515a8e4cbb1e5a3bde8cfc7640
ricegood <951227@gmail.com>
2017-02-12 21:42:54 +0900

    Opening Story

---
+++ Assets/Fade.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour {
+
+	public GameObject thisObject;
+	public GameObject nextObject;
+	public string nextScene;
+	private Text thisText;
+	private float now;
+	private float end;
+
+	// Use this for initialization
+	void Start () {
+		thisText = thisObject.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (now <= 5) {
+			now += Time.deltaTime;
+			thisText.color = new Color (1f, 1f, 1f, now / 2);
+		}
+
+		else if (now >= 5 && end < 2) {
+			end += Time.deltaTime;
+			thisText.color = new Color (1f, 1f, 1f, 1 - end/2);
+		}
+
+		else if (end >= 2) {
+			if (nextScene != "") {
+				SceneManager.LoadScene ("Main");
+			} else {
+				thisObject.SetActive (false);
+				nextObject.SetActive (true);
+			}
+		}
+	}
+}
---
+++ Assets/Story.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Story : MonoBehaviour {
+	public Text text;
+	public GameObject firstLine;
+
+	// Use this for initialization
+	void Start () {
+		text.text = "In the inauguration speech, president " + Character.getName() + " vowed to transform Korea into an economic powerhouse through sustainable development...";
+		firstLine.SetActive (true);
+	}
+}
