d9aa5aeafbe83424d00cc1a208a60f56bb92128d
ricegood <951227@gmail.com>
2017-02-13 14:18:41 +0900

    Story Scene Next Button

--- Assets/Script/Fade.cs
+++ Assets/Script/Fade.cs
@@ -8,6 +8,7 @@ public class Fade : MonoBehaviour {
 
 	public GameObject thisObject;
 	public GameObject nextObject;
+	public Image NextButton;
 	public string nextScene;
 	private Text thisText;
 	private float now;
@@ -20,20 +21,23 @@ public class Fade : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (now <= 5) {
+		if (now <= 3) {
 			now += Time.deltaTime;
 			thisText.color = new Color (1f, 1f, 1f, now / 2);
+			NextButton.color = new Color (1f, 1f, 1f, now / 2);
 		}
 
-		else if (now >= 5 && end < 2) {
+		else if (now >= 3 && end < 3) {
 			end += Time.deltaTime;
 			thisText.color = new Color (1f, 1f, 1f, 1 - end/2);
+			NextButton.color = new Color (1f, 1f, 1f, 1 - end/2);
 		}
 
-		else if (end >= 2) {
+		else if (end >= 3) {
 			if (nextScene != "") {
 				SceneManager.LoadScene ("Main");
 			} else {
+				Story.page++;
 				thisObject.SetActive (false);
 				nextObject.SetActive (true);
 			}
--- Assets/Script/Story.cs
+++ Assets/Script/Story.cs
@@ -5,11 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class Story : MonoBehaviour {
 	public Text text;
-	public GameObject firstLine;
+	public GameObject[] lines;
+	public static int page;
 
 	// Use this for initialization
 	void Start () {
+		page = 0;
 		text.text = "In the inauguration speech, president " + Character.getName() + " vowed to transform Korea into an economic powerhouse through sustainable development...";
-		firstLine.SetActive (true);
+		lines[0].SetActive (true);
+	}
+
+	public void nextPage(){
+		if (page < 2) {
+			lines [page++].SetActive (false);
+			lines [page].SetActive (true);
+		} else if (page == 2) {
+			SceneManager.LoadScene ("Main");
+		}
 	}
 }
